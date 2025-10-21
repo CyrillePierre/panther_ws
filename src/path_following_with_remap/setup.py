dd87ac87e7c348fb9e9d0330358a89515d0ead69
lemairelouis <louis.lemaire@uliege.be>
2025-10-21 15:32:31 +0200

    add path_following_with_remap package
    Launch path_following from tirrex_demo + remap topics with topic_tools relay

---
+++ src/path_following_with_remap/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'path_following_with_remap'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='you@example.com',
+    description='Wrapper launch file that adds cmd_vel remapping to romea_path_following',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [],
+    },
+)
