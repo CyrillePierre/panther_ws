0c2da4325d31ba6d7f2f333004d74831521a7f03
sylvain <sylvain.dewez@uliege.be>
2025-11-10 10:10:09 +0000

    Add path preprocessing depending on FP_ENU0
    FP_ENU0 is used as frame of reference, but its position changes at FP VRTK Fusion initialization. The preprocessing script listens to FP_ECEF->FP_ENU0 and changes the wgs84 anchor and traj file accordingly

--- src/path_following_with_remap/launch/path_following_with_remap.launch.py
+++ src/path_following_with_remap/launch/path_following_with_remap.launch.py
@@ -1,10 +1,10 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler, LogInfo
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-
+from launch.event_handlers import OnProcessExit
 
 def generate_launch_description():
     # Declare launch arguments
@@ -30,9 +30,29 @@ def generate_launch_description():
         }.items(),
     )
 
+    preprocess_node = Node(
+        package='path_following_with_remap',
+        executable='preprocess_node',
+        name='path_following_preprocess_node',
+        parameters=[{
+            'robot_config_directory': LaunchConfiguration('robot_config_directory'),
+            'trajectory_filename': LaunchConfiguration('trajectory_filename'),
+        }],
+        output='screen',
+    )
+
+    start_path_follower_after_preproc = RegisterEventHandler(
+        OnProcessExit(
+            target_action=preprocess_node,
+            on_exit=[
+                LogInfo(msg='[LAUNCH] Preprocess finished, starting robot_path_following.launch.py'),
+                base_launch],
+        )
+    )
+
     # Relay node to remap cmd_vel
-    source_topic = str(LaunchConfiguration('robot_namespace')) + '/base/controller/cmd_skid_steering'
-    target_topic = str(LaunchConfiguration('robot_namespace')) + '/cmd_vel'
+    source_topic = '/panther/path_following/cmd_vel'
+    target_topic = '/panther/cmd_vel'
     cmd_vel_relay = Node(
         package='topic_tools',
         executable='relay',
@@ -46,7 +66,8 @@ def generate_launch_description():
 
     return LaunchDescription(
         declared_arguments + [
-            base_launch,
             cmd_vel_relay,
+            preprocess_node,
+            start_path_follower_after_preproc,
         ]
     )
